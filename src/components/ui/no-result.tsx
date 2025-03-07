70bcc4751e5c21b4711f73e7092189b9a4c593ab
YammD123 <siamqueen52@gmail.com>
2025-03-07 18:44:19 +0700

    kwkwkw

--- src/components/ui/no-result.tsx
+++ src/components/ui/no-result.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { Search } from "lucide-react"
 
 export default function NoResult() {
   return (
-    <div className='flex items-center justify-center h-full w-full'>
-        Tidak Ada Product Yang di Temukan
-    </div>
-  )
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+      className="flex flex-col items-center justify-center h-full w-full text-lg text-gray-500 gap-4"
+    >
+      <Search className="w-12 h-12 text-gray-400 animate-bounce" />
+      Tidak Ada Product Yang Ditemukan
+    </motion.div>
+  );
 }
